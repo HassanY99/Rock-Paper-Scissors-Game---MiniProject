f2cd6412f07cd7ae1cb32e55cc5607a9ba2a2d78
Hassan Yusuf <hassanyousuf1999@gmail.com>
2021-11-19 21:03:29 -0500

    The game is fully functional

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let options = ['R', 'P', 'S'];
 
 
 var playGame = function() {
+
+    // User Choice
     let userChoice = window.prompt('Choose R, P or S');
 
     if(!userChoice) {
@@ -27,20 +29,42 @@ var playGame = function() {
     let computerChoice = options[index];
 
     window.alert("Computer chose: " + computerChoice);
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
+    // Using If/Else Statements to figure out the results
+    
+    if(userChoice === computerChoice) {
+        ties++;
+        window.alert("It was a tie!"); 
+    } else if(
+    (userChoice === "R" && computerChoice === 'S') || 
+    (userChoice === "S" && computerChoice === 'P') || 
+    (userChoice === "P" && computerChoice === 'R')
+    ) {
+        wins++;
+        window.alert("You Winn!")
+    } else {
+        losses++;
+        window.alert('You lost!')
+    }
+    
+    
+    // Print Stats
+    
+    window.alert(`Stats: 
+    Wins: ${wins}
+    Losses: ${losses}
+    Ties: ${ties}`);
+    
+    
+    // Ask if they want to play again?
+    
+    var playAgain = window.confirm("Do you want to play again?");
+    
+    if(playAgain) {
+        playGame();
+    }
+};
 
 
 // ======= Initialization ======== //
